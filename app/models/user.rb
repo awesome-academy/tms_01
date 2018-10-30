e39b423b3548dc4872b738418bdaecf3f42e25ac
nguyenhuan1994dn <nguyenhuan1994dn@gmail.com>
2018-10-30 14:40:57 +0700

    Log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save{email.downcase!}
   has_many :user_courses, dependent: :destroy
   has_many :courses, through: :user_courses
@@ -25,4 +26,31 @@ class User < ApplicationRecord
     length: {maximum: Settings.user.address_max_length}
 
   enum role: {trainee: 0, supervisor: 1, admin: 2}
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
 end
